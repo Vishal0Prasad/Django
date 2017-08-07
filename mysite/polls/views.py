e3c780f2977942423fa186d2df860a0db40a3d5a
Vishal0Prasad <30768888+Vishal0Prasad@users.noreply.github.com>
2017-08-07 17:42:57 +0000

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,31 +9,30 @@ from .models import Question
 from django.template import loader
 from django.http import Http404
 
-def index(request):
+'''def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	template = loader.get_template('/polls/ndex.html')
 	context={
 		'latest_question_list': latest_question_list,
 	}
-	return HttpResponse(template.render(context,request))
+	return HttpResponse(template.render(context,request))'''
 
-'''OR
+
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	context={'latest_question_list': latest_question_list,}
 	return render(request,/polls.index.html',context)
-'''
-def detail(request,question_id):
+
+'''def detail(request,question_id):
 	try:
 		question = Question.objects.get(question_id)
 	except Question.DoesNotExist:
 		raise Http404("Question does not exist")
-	return render(request,'polls/detail.html',{'question': question})
-'''OR
+	return render(request,'polls/detail.html',{'question': question})'''
+		      
 def detail(request,question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	return render(request,'plss/detail.html',{'question':question})
-'''
 
 def results(request,question_id):
 	return HttpResponse("You're lookng at the results of the question %s." %question_id)
